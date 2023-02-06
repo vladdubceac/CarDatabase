64554fd1c9546adfe6ac9bf30fbcc31429928279
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-06 23:05:07 +0200

    Modified properties

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,17 @@
 package com.packt.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CardatabaseApplication.class);	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
+		LOGGER.info("Application started");
 	}
 
 }
